1cb2746c5662c3467a8def21a570d9ca65b6974f
ItayMizrachi <itaymiz98@gmail.com>
2023-07-27 06:02:13 +0300

    comment user links

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -102,13 +102,15 @@ const Post = ({ _id, user_name, img_url, desc, profilePic }) => {
         <div className="h-20 ml-10 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
           {commentsInfo.map((comment) => (
             <div key={comment._id} className="flex items-center mb-3 space-x-2">
+              <Link to={comment.user.user_name}>
               <img
                 className="rounded-full h-7"
                 src={comment.user.profilePic}
                 alt="profile pic"
               />
+              </Link>
               <p className="flex-1 text-sm">
-                <span className="font-bold">{comment.user.user_name} </span>{" "}
+                <Link to={comment.user.user_name} className="font-bold">{comment.user.user_name} </Link>
                 {comment.text}
               </p>
               <p className="pr-5 text-xs">5 days ago {comment.date_created}</p>
