25de98a6968a7f426e704a21d8c0fb7600616d80
ItayMizrachi <itaymiz98@gmail.com>
2023-08-12 17:47:42 +0300

    admin-omri

--- src/admin/PostsAdmin.jsx
+++ src/admin/PostsAdmin.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { URL, doApiGet, doApiMethod } from '../services/apiService';
-import PagesBtns from './PagesBtns';
+import { Card } from "@material-tailwind/react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { URL, doApiGet, doApiMethod } from "../services/apiService";
+import PagesBtns from "./PagesBtns";
 
-const PostsAdmin = () => {
+const UsersList = () => {
+  const nav = useNavigate();
   const [query] = useSearchParams();
   const [ar, setAr] = useState([]);
 
@@ -13,7 +15,7 @@ const PostsAdmin = () => {
 
   const doApi = async () => {
     const page = query.get("page") || 1;
-    const url = URL + "/userPosts/postsList?page=" + page;
+    const url = URL + "/users/usersList?page=" + page;
     try {
       const data = await doApiGet(url);
       console.log(data);
@@ -23,10 +25,23 @@ const PostsAdmin = () => {
     }
   };
 
-  const deletePost = async (_id) => {
+  const changeRole = async (userInfo) => {
+    const newRole = userInfo.role == "admin" ? "user" : "admin";
+    try {
+      const url = `${URL}/users/changeRole/${userInfo._id}/${newRole}`;
+      const data = await doApiMethod(url, "PATCH");
+      if (data.modifiedCount) {
+        doApi();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteUser = async (_id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       try {
-        const url = `${URL}/userPosts/${_id}`;
+        const url = `${URL}/users/${_id}`;
         const data = await doApiMethod(url, "DELETE");
 
         if (data.deletedCount) {
@@ -37,52 +52,79 @@ const PostsAdmin = () => {
       }
     }
   };
+
   return (
     <div className="container">
-      <h1 className="display-4 ">ALL posts List</h1>
+      <h1>Users List</h1>
       <PagesBtns
-        apiUrl={URL + "/userPosts/count"}
-        linkTo={"/admin/posts?page="}
-      />
-      <table className="table table-striped my-3">
-        <thead>
-          <tr>
-            <td>#</td>
-            <td>user_name</td>
-            <td>name</td>
-            <td>_id</td>
-            <td>description</td>
-            <td>delete</td>
-          </tr>
-        </thead>
-        <tbody>
-          {ar.map((item, i) => {
-            const page = query.get("page") || 1;
-            return (
-              <tr key={i + 1}>
-                <td>{(page - 1) * 5 + i + 1}</td>
-                <td>{item.user_name}</td>
-                <td>{item.name}</td>
-                <td>{item._id}</td>
-                <td>{item.description}</td>
-
-                <td>
-                  <button
-                    onClick={() => {
-                      deletePost(item._id);
-                    }}
-                    className="btn btn-danger"
-                  >
-                    DEL
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        apiUrl={URL + "/users/count"}
+        linkTo={"/admin/users?page="} />
+      <Card className="h-full w-full overflow-scroll">
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>user_name</td>
+              <td>name</td>
+              <td>email</td>
+              <td>_id</td>
+              <td>role</td>
+              <td>delete</td>
+            </tr>
+          </thead>
+          <tbody>
+            {ar.map((item, i) => {
+              const page = query.get("page") || 1;
+              return (
+                <tr key={i + 1}>
+                  <td>{(page - 1) * 5 + i + 1}</td>
+                  <td>{item.user_name}</td>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item._id}</td>
+                  <td>
+                    <button
+                      style={{
+                        background: item.role == "admin" ? "orange" : "green",
+                      }}
+                      onClick={() => {
+                        changeRole(item);
+                      }}
+                      className="btn"
+                    >
+                      {item.role}
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        deleteUser(item._id);
+                      }}
+                      className="btn"
+                    >
+                      DEL
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        if (item._id != "64c27124871892c1bd068dc6") {
+                          nav("/admin/users/edit/" + item._id);
+                        }
+                      }}
+                      className="btn"
+                    >
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Card>
     </div>
   );
 };
 
-export default PostsAdmin;
+export default UsersList;
